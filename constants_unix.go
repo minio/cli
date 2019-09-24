9547ffee386d2f346ecf20790d9fbae5341f5b80
Ersan <ersan.bozduman@gmail.com>
2019-09-24 14:48:34 -0700

    Adds missing prompt to Unix style

--- constants_unix.go
+++ constants_unix.go
@@ -7,6 +7,6 @@ const (
 	defaultPrompt             = "$"
 	defaultEnvSetCmd          = "export"
 	defaultAssignmentOperator = "="
-	defaultDisableHistory     = "set +o history"
-	defaultEnableHistory      = "set -o history"
+	defaultDisableHistory     = "$ set +o history"
+	defaultEnableHistory      = "$ set -o history"
 )
